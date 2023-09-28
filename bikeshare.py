4fc480ced335ee19ce4b6034e4e3a51e084ef043
jakechevrier <jakechevrier@gmail.com>
2023-09-27 22:44:56 -0700

    added thank you if program ended

--- bikeshare.py
+++ bikeshare.py
@@ -289,6 +289,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("Thank you.")
             break
 
 
